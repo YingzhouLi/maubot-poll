2bb8ae0cc658e139d146e3733fd1c33c0dc649f4
Yingzhou Li <yingzhouli0417@gmail.com>
2022-02-01 21:32:01 +0800

    Wrap code

--- polls/database.py
+++ polls/database.py
@@ -8,14 +8,14 @@ from .types import Poll
 
 
 def _generate_random_string() -> str:
-    return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    return ''.join(random.SystemRandom().choice(string.ascii_uppercase \
+            + string.digits) for _ in range(6))
 
 
 class PollDatabase:
     db: Engine
     polls: Table
     choices: Table
-
     votes: Table
 
     def __init__(self, db: Engine):
@@ -23,57 +23,73 @@ class PollDatabase:
 
         meta = MetaData()
         meta.bind = db
-
         self.polls = Table("polls", meta,
-                           Column("id", Integer, primary_key=True, autoincrement=True),
-                           Column("code", String(6), nullable=False),
-                           Column("creator", String(255), nullable=False),
-                           Column("room_id", String(255), nullable=False),
-                           Column("question", Text, nullable=False))
+                         Column("id", Integer, primary_key=True,
+                             autoincrement=True),
+                         Column("code", String(6), nullable=False),
+                         Column("creator", String(255), nullable=False),
+                         Column("room_id", String(255), nullable=False),
+                         Column("question", Text, nullable=False))
+
         self.choices = Table("choices", meta,
-                             Column("id", Integer, primary_key=True, autoincrement=True),
-                             Column("poll_id", Integer, nullable=False),
-                             Column("choice_number", Integer, nullable=False),
-                             Column("content", Text, nullable=False))
+                           Column("id", Integer, primary_key=True,
+                               autoincrement=True),
+                           Column("poll_id", Integer, nullable=False),
+                           Column("choice_number", Integer, nullable=False),
+                           Column("content", Text, nullable=False))
 
         self.votes = Table("votes", meta,
-                           Column("id", Integer, primary_key=True, autoincrement=True),
-                           Column("poll_id", Integer, nullable=False),
-                           Column("choice_id", Integer, nullable=False),
-                           Column("voter", String(255), nullable=False))
+                         Column("id", Integer, primary_key=True,
+                             autoincrement=True),
+                         Column("poll_id", Integer, nullable=False),
+                         Column("choice_id", Integer, nullable=False),
+                         Column("voter", String(255), nullable=False))
         meta.create_all()
 
-    def create_poll(self, question: str, choices: list, creator: str, room_id: str) -> str:
+    def create_poll(self,
+            question: str, choices: list, creator: str, room_id: str
+            ) -> str:
         code = _generate_random_string()
         proxy = self.db.execute(
-            self.polls.insert().values(code=code, creator=creator, question=question, room_id=room_id))
+            self.polls.insert().values(code=code, creator=creator,
+                question=question, room_id=room_id)
+            )
         for index, choice in enumerate(choices):
             self.db.execute(
-                self.choices.insert().values(poll_id=proxy.inserted_primary_key[0], choice_number=index + 1,
-                                             content=choice))
+                self.choices.insert().values(
+                    poll_id=proxy.inserted_primary_key[0],
+                    choice_number=index + 1, content=choice))
         return code
 
     def get_poll(self, room_id: str, code: str):
         poll = self.db.execute(
-            self.polls.select().where(self.polls.c.room_id == room_id).where(self.polls.c.code == code)).fetchone()
+            self.polls.select().where(
+                self.polls.c.room_id == room_id).where(
+                    self.polls.c.code == code)).fetchone()
         if poll is None:
             return Poll(None, None, None)
         return Poll(poll.id, poll.question, poll.creator)
 
     def get_poll_choices_ids(self, poll_id: int):
-        proxy = self.db.execute(self.choices.select().where(self.choices.c.poll_id == poll_id))
+        proxy = self.db.execute(self.choices.select().where(
+            self.choices.c.poll_id == poll_id))
         choices = {}
         for row in proxy:
             choices[row.choice_number] = row.id
         return choices
 
     def get_poll_choices(self, poll_id: int):
-        return self.db.execute(self.choices.select().where(self.choices.c.poll_id == poll_id))
+        return self.db.execute(self.choices.select().where(
+            self.choices.c.poll_id == poll_id))
 
     def set_vote(self, poll_id: int, choice_id: int, user_id: str):
         self.db.execute(
-            self.votes.delete().where(self.votes.c.poll_id == poll_id).where(self.votes.c.voter == user_id))
-        self.db.execute(self.votes.insert().values(poll_id=poll_id, choice_id=choice_id, voter=user_id))
+            self.votes.delete().where(
+                self.votes.c.poll_id == poll_id).where(
+                    self.votes.c.voter == user_id))
+        self.db.execute(self.votes.insert().values(poll_id=poll_id,
+            choice_id=choice_id, voter=user_id))
 
     def get_votes(self, poll_id: int):
-        return self.db.execute(self.votes.select().where(self.votes.c.poll_id == poll_id)).fetchall()
+        return self.db.execute(self.votes.select().where(
+            self.votes.c.poll_id == poll_id)).fetchall()
--- polls/pollplugin.py
+++ polls/pollplugin.py
@@ -14,33 +14,42 @@ def _remove_suffix(input_string, suffix):
     return input_string
 
 
-def _generate_poll_html_message(question: str, choices: list, code: str) -> str:
+def _generate_poll_html_message(
+        question: str, choices: list, code: str) -> str:
     message = f"<p><em>{question}</em></p>\n<ol>\n"
     for choice in choices:
         message = message + f"<li>{choice}</li>\n"
-    message = message + f"\n</ol>\n<p><em>Voting command: </em><code>!vote {code} &lt;Nummer&gt;</code></p>"
+    message = message + f"\n</ol>\n" \
+            f"<p><em>Voting command: </em>" \
+            f"<code>!vote {code} &lt;Nummer&gt;</code></p>"
     return message
 
 
-def _generate_poll_text_message(question: str, choices: list, code: str) -> str:
+def _generate_poll_text_message(
+        question: str, choices: list, code: str) -> str:
     message = f"{question}\n\n"
     for index, choice in enumerate(choices):
         message = message + f"{index}. {choice}\n"
     message = message + f"\nVoting command: !vote {code} <Nummer>"
     return message
 
-def _generate_result_html_message(question: str, choices: list, total_votes: int) -> str:
+def _generate_result_html_message(
+        question: str, choices: list, total_votes: int) -> str:
     message = f"<h4>Poll result: <em>{question}</em></h4><ol>"
     for choice in choices:
-        message = message + f"<li>{choice.content}<br><em>{len(choice.votes)} in {total_votes} " \
-                            f"Votes - {'{:.0%}'.format(len(choice.votes) / total_votes)}</em></li>"
+        message = message + f"<li>{choice.content}" \
+                f"<br><em>{len(choice.votes)} in {total_votes} Votes - " \
+                f"{'{:.0%}'.format(len(choice.votes) / total_votes)}" \
+                f"</em></li>"
     return message + "</ol>"
 
-def _generate_result_text_message(question: str, choices: list, total_votes: int) -> str:
+def _generate_result_text_message(
+        question: str, choices: list, total_votes: int) -> str:
     message = f"Poll result: {question}\n"
     for choice in choices:
-        message = message + f"{choice.number[0]}. {choice.content}\n ({len(choice.votes)} in {total_votes} " \
-                            f"Votes - {'{:.0%}'.format(len(choice.votes) / total_votes)})\n"
+        message = message + f"{choice.number[0]}. {choice.content}\n"
+                f"({len(choice.votes)} in {total_votes} Votes - " \
+                f"{'{:.0%}'.format(len(choice.votes) / total_votes)})\n"
     return message
 
 
@@ -50,11 +59,14 @@ class PollPlugin(Plugin):
     async def start(self) -> None:
         self.db = PollDatabase(self.database)
 
-    async def _send_temporary_response(self, content: str, evt: MessageEvent, delay: int = 7):
+    async def _send_temporary_response(self,
+            content: str, evt: MessageEvent, delay: int = 7):
         event = await evt.respond(content)
         time.sleep(delay)
-        levels = await self.client.get_state_event(evt.room_id, EventType.ROOM_POWER_LEVELS)
-        power_levels = await self.client.get_state_event(evt.room_id, EventType.ROOM_POWER_LEVELS)
+        levels = await self.client.get_state_event(evt.room_id,
+                EventType.ROOM_POWER_LEVELS)
+        power_levels = await self.client.get_state_event(evt.room_id,
+                EventType.ROOM_POWER_LEVELS)
         if levels.get_user_level(self.client.mxid) >= power_levels.redact:
             await self.client.redact(evt.room_id, evt.event_id)
         await self.client.redact(evt.room_id, event)
@@ -64,9 +76,7 @@ class PollPlugin(Plugin):
         votes = self.db.get_votes(poll_id)
 
         total_votes = 0
-
         data = {}
-
         for choice in choices:
             data[choice.id] = Choice(choice.choice_number, choice.content)
         for vote in votes:
@@ -82,13 +92,19 @@ class PollPlugin(Plugin):
         return total_votes, choices
 
     @command.new(name="poll", require_subcommand=True)
+    
     async def poll_command(self, evt: MessageEvent):
         pass
 
-    @poll_command.subcommand(name="create", aliases=["new"],
-                             help="Create a new poll with the following command: "
-                                  "`!poll create <Question> | <Option 1> | <Option 2> ...`")
-    @command.argument(name="content", label="content", pass_raw=True, required=True)
+    @poll_command.subcommand(
+            name="create", aliases=["new"],
+            help="Create a new poll with the following command: " \
+                    "`!poll create <Question> " \
+                    "| <Option 1> | <Option 2> ...`")
+
+    @command.argument(
+            name="content", label="content", pass_raw=True, required=True)
+
     async def create_poll(self, evt: MessageEvent, content: str):
         if content is not "":
             raw_parts = content.split("|")
@@ -96,20 +112,27 @@ class PollPlugin(Plugin):
             for part in raw_parts:
                 parts.append(part.strip())
             if len(parts) < 3:
-                evt.respond("Please enter at least 2 options: `!poll create <Question> | <Option 1> | <Option 2> ...`")
+                evt.respond("Please enter at least 2 options: " \
+                        "`!poll create <Question> " \
+                        "| <Option 1> | <Option 2> ...`")
             else:
                 question = parts[0]
                 parts.pop(0)
-                code = self.db.create_poll(question, parts, evt.sender, evt.room_id)
-                await self.client.send_text(evt.room_id, _generate_poll_text_message(question, parts, code),
-                                            _generate_poll_html_message(question, parts, code))
+                code = self.db.create_poll(question, parts, evt.sender,
+                        evt.room_id)
+                await self.client.send_text(evt.room_id,
+                        _generate_poll_text_message(question, parts, code),
+                        _generate_poll_html_message(question, parts, code))
         else:
-            await evt.respond(
-                "Please provide the content for the poll: `!poll create <Question> | <Option 1> | <Option 2> ...`")
+            await evt.respond("Please provide the content for the poll: " \
+                            "`!poll create <Question> " \
+                            "| <Option 1> | <Option 2> ...`")
 
     @command.new(name="vote", help="Take a poll..")
-    @command.argument(name="code", label="Code", pass_raw=False, required=True)
-    @command.argument(name="choice", label="Option", pass_raw=False, required=True)
+    @command.argument(
+            name="code", label="Code", pass_raw=False, required=True)
+    @command.argument(
+            name="choice", label="Option", pass_raw=False, required=True)
     async def vote_poll(self, evt: MessageEvent, code: str, choice: str):
         poll = self.db.get_poll(evt.room_id, code)
         if poll.exists:
@@ -117,47 +140,64 @@ class PollPlugin(Plugin):
             try:
                 choice_id = choices[int(choice)]
                 self.db.set_vote(poll.id, choice_id, evt.sender)
-                await self._send_temporary_response(f"{evt.sender}, voted for option {choice}.", evt)
+                await self._send_temporary_response(
+                        f"{evt.sender}, voted for option {choice}.", evt)
             except KeyError:
-                await self._send_temporary_response("There is no such option.", evt)
+                await self._send_temporary_response(
+                        "There is no such option.", evt)
             except ValueError:
-                await self._send_temporary_response("Please enter a valid option!", evt)
+                await self._send_temporary_response(
+                        "Please enter a valid option!", evt)
         else:
-            await self._send_temporary_response("This poll does not exist!", evt)
+            await self._send_temporary_response(
+                    "This poll does not exist!", evt)
 
-    @poll_command.subcommand("result", help="Shows the result to the creator of the poll.")
-    @command.argument(name="code", label="Code", pass_raw=False, required=True)
+    @poll_command.subcommand(
+            name="result",
+            help="Shows the result to the creator of the poll.")
+    @command.argument(
+            name="code", label="Code", pass_raw=False, required=True)
     async def poll_result(self, evt: MessageEvent, code: str):
         poll = self.db.get_poll(evt.room_id, code)
         if not poll.exists:
-            await self._send_temporary_response("This poll does not exist!", evt)
+            await self._send_temporary_response(
+                    "This poll does not exist!", evt)
             return
 
         if poll.creator.strip() != evt.sender.strip():
-            await self._send_temporary_response("Only the creator of the poll can view the results!", evt)
+            await self._send_temporary_response(
+                    "Only the creator of the poll can view the results!",
+                    evt)
             return
 
         data = self._sort_choices(poll.id)
         total_votes = data[0]
         choices = data[1]
 
-        await self.client.send_text(evt.room_id, _generate_result_text_message(poll.question, choices, total_votes),
-                                    _generate_result_html_message(poll.question, choices, total_votes))
+        await self.client.send_text(evt.room_id,
+                _generate_result_text_message(poll.question,
+                    choices, total_votes),
+                _generate_result_html_message(poll.question,
+                    choices, total_votes))
 
-    @poll_command.subcommand("ping",
-                             help="Pings the participants of a poll who voted for the specified option.")
-    @command.argument(name="code", label="Code", pass_raw=False, required=True)
-    @command.argument(name="option", label="Option", pass_raw=False, required=True)
+    @poll_command.subcommand(
+            name="ping", help="Pings the participants of a poll " \
+                                "who voted for the specified option.")
+    @command.argument(
+            name="code", label="Code", pass_raw=False, required=True)
+    @command.argument(
+            name="option", label="Option", pass_raw=False, required=True)
     async def ping_poll(self, evt: MessageEvent, code: str, option: str):
         poll = self.db.get_poll(evt.room_id, code)
         if not poll.exists:
-            await self._send_temporary_response("This poll does not exist!", evt)
+            await self._send_temporary_response(
+                    "This poll does not exist!", evt)
             return
-
         if poll.creator.strip() != evt.sender.strip():
-            await self._send_temporary_response("Only the creator of the poll can ping participants!", evt)
+            await self._send_temporary_response(
+                    "Only the creator of the poll can ping participants!",
+                    evt)
             return
-
         try:
             opt = int(option)
             choices = self._sort_choices(poll.id)[1]
@@ -170,4 +210,5 @@ class PollPlugin(Plugin):
                     return
             raise ValueError
         except ValueError:
-            await self._send_temporary_response("You must specify a valid option!", evt)
+            await self._send_temporary_response(
+                    "You must specify a valid option!", evt)
